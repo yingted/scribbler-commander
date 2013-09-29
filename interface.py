6b4a9b8db5d1a2081a67698d8667150b3cd72490
yingted@gmail.com <yingted@gmail.com@2a0f7078-9aa0-a7c8-291c-ce4e4205d19b>
2013-09-29 03:18:10 +0000

    make some buttons work

--- interface.py
+++ interface.py
@@ -1,14 +1,51 @@
 #!/usr/bin/python
 import cherrypy
 import os
+import myro
+import socket
+
+# connect to myro
+try:
+	s=socket.socket()
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	# test if simulator is running
+	s.bind(("127.0.0.1", 60000))
+	s.close()
+except socket.error, e:
+	# simulator is running, so wait and connect to it
+	from myro.robots.simulator import SimScribbler
+	robot = myro.globvars.robot = SimScribbler(None)
+else:
+	# start a new simulator
+	myro.simulator()
 
 class ScribblerCommander(object):
-	pass
+	@cherrypy.expose
+	def index(self, do=None):
+		# check if action is safe
+		if do in ("forward", "backward", "stop", "left", "right"):
+			# TODO replace with a proper RPC mechanism
+			# and expose REST API
+			getattr(self,do)()
+		# switch POST to a GET
+		raise cherrypy.HTTPRedirect('/')
+	# some simple methods, which will be decorated later
+	def forward(self):
+		myro.forward(1)
+	def backward(self):
+		myro.backward(1)
+	def left(self):
+		myro.turn(1)
+	def right(self):
+		myro.turn(-1)
+	def stop(self):
+		myro.stop()
 
 cherrypy.quickstart(ScribblerCommander(),config={
 	'/' : {
 		'tools.staticfile.root' : os.path.dirname(os.path.abspath(__file__)),
 		'tools.staticfile.on' : True,
 		'tools.staticfile.filename' : 'index.html',
+		'tools.staticfile.match' : '^/$', # only match the exact url /
 	}
 })
