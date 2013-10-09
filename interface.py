7a108765b0a42a51f41ad5b614ad290e9c724a83
yingted@gmail.com <yingted@gmail.com@2a0f7078-9aa0-a7c8-291c-ce4e4205d19b>
2013-10-09 12:31:42 +0000

    remove robotConnected code

--- interface.py
+++ interface.py
@@ -3,7 +3,7 @@ import cherrypy
 import os
 import myro
 import socket
-robotConnected = 1
+
 # connect to myro
 try:
 	s=socket.socket()
@@ -14,11 +14,9 @@ try:
 except socket.error, e:
 	# simulator is running, so wait and connect to it
 	robot = myro.globvars.robot = myro.robots.simulator.SimScribbler(None)
-	robotConnected = 0
 else:
 	# start a new simulator
 	myro.simulator()
-	robotConnected = 0
 
 if not hasattr(robot, "robotinfo"): # prevent KeyError on KeyboardInterrupt
 	robot.robotinfo = {}
@@ -46,10 +44,7 @@ class ScribblerCommander(object):
 		myro.stop()
 	@cherrypy.expose
 	def battery(self):
-		if robotConnected == 1:
-			return str(myro.getBattery()/9.00)
-		else:
-			return "Robot unconnected"
+		return str(myro.getBattery()/9.)
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 cherrypy.quickstart(ScribblerCommander(),config={
