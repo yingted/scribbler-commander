756255b622098405d308b55a3aa04765d57d9787
yingted@gmail.com <yingted@gmail.com@2a0f7078-9aa0-a7c8-291c-ce4e4205d19b>
2013-11-06 06:37:38 +0000

    create prior distribution

--- trunk/make-prior.py
+++ trunk/make-prior.py
@@ -1,39 +1,46 @@
-from util import *
-from sys import argv
-from pickle import dump
-from time import time
-f=open(argv[1],'w')
-xp_initialize()
-out=[]
-for irp in xrange(122,147,3):
-	robot.setIRPower(irp)
-	get_obstacle(7)
-	while True:
-		data=[]
-		raw_input('press enter to start %d: '%irp)
-		try:
-			end=time()+50
-			motors(-.1,-.1)
-			while True:
-				t1=time()
-				val=get_obstacle('center')
-				t2=time()
-				data.append({
-					't':(t1+t2)/2,
-					'v':val,
-				})
-				if t2>=end:
-					break
-			stop()
-			print'ir power:',irp,'samples:',len(data)
-			break
-		except KeyboardInterrupt:
-			stop()
-	out.append({
-		'irp':irp,
-		'data':data,
-	})
-	f.seek(0)
-	dump(out,f)
-	f.flush()
-f.close()
+#!/usr/bin/env python
+"""generates prior from data"""
+N=50
+if __name__=="__main__":
+	from pickle import load,dump
+	from sys import argv
+	from numpy import *
+	from scipy.stats import linregress
+	data={}
+	with open(argv[1])as f:
+		for ent in load(f):
+			start=ent['data'][0]['t']*1.5-.5*ent['data'][1]['t']
+			t=[]
+			v=[]
+			for sample in ent['data']:
+				t.append(sample['t']-start)
+				v.append(sample['v'])
+			t=array(t)
+			v=array(v)
+			weight=zeros(N+1)
+			vals=[]
+			dx=1
+			for start in xrange(0,N,dx):
+				i=(start<=t)&(t<start+dx)
+				m_t,b_t,_,_,sigma_t=linregress(arange(len(t))[i],t[i])
+				m_v,b_v,_,_,sigma_v=linregress(t[i],v[i])
+				var_t=(sigma_t*m_v)**2+1e-1
+				var_v=sigma_v**2+1e-1
+				w=1/(var_t+var_v)
+				vals.append((m_v*start+b_v,m_v*(start+1)+b_v,w))
+				weight[start]+=w
+				weight[start+1]+=w
+			i=arange(N+1)
+			sigma=weight**-.5
+
+			d=zeros(N+1)
+			for i,(left,right,w)in enumerate(vals):
+				d[dx*i]+=left*w
+				d[dx*(i+1)]+=right*w
+			d/=weight
+			data[ent['irp']]={
+				'd':d,
+				'sigma':sigma,
+			}
+	with open(argv[2],'w')as f:
+		dump(data,f)
