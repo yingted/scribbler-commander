a1a620b55478d547f74f6410fbc151b52dee3ed8
yingted@gmail.com <yingted@gmail.com@2a0f7078-9aa0-a7c8-291c-ce4e4205d19b>
2013-11-22 01:32:57 +0000

    rewrite ajax

--- trunk/interface.py
+++ trunk/interface.py
@@ -4,35 +4,58 @@ import os
 import myro
 import util
 import sys
+import itertools
 util.connect_async()
+def jsonify(f):
+	def wrapped(*args, **kwargs):
+		val = f(*args, **kwargs)
+		response.headers['Content-Type'] = 'application/json'
+		return json.dumps(val)
+	return wrapped
+def ajax(handler):
+	return cherrypy.expose(cherrypy.tools.allow(methods=('POST',))(cherrypy.tools.json_out()(handler)))
 class ScribblerCommander(object):
 	@cherrypy.expose
 	def index(self, do=None):
 		# check if action is safe
-		if do in ("forward", "backward", "stop", "left", "right"):
-			# TODO replace with a proper RPC mechanism
-			# and expose REST API
-			getattr(self,do)()
+		if hasattr(self, do):
+			handler = getattr(self, do)
+			if hasattr(handler, 'exposed') and handler.exposed:
+				getattr(self, do)()
 		# switch POST to a GET
 		raise cherrypy.HTTPRedirect('/')
-	# some simple methods, which will be decorated later
+	@ajax
 	def forward(self):
 		myro.forward(1)
+	@ajax
 	def backward(self):
 		myro.backward(1)
+	@ajax
 	def left(self):
 		myro.turn(1)
+	@ajax
 	def right(self):
 		myro.turn(-1)
+	@ajax
 	def stop(self):
 		myro.stop()
-	@cherrypy.expose
+	@ajax
 	def battery(self):
 		try:
-			return str(myro.getBattery()/9.)
+			return myro.getBattery()/9.
 		except AttributeError:
 			raise cherrypy.HTTPError(503, 'Service Unavailable')
-
+	@cherrypy.expose
+	@cherrypy.tools.allow(methods=('POST',))
+	def subscribe(self):
+		"""subscribe to events"""
+		class gen(object):
+			def __iter__(self):
+				return self
+			def next(self):
+				myro.wait(10**10)
+		return itertools.chain((), gen())
+	subscribe._cp_config = {'response.stream' : True}
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 cherrypy.quickstart(ScribblerCommander(),config={
