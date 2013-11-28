e610f585ae685372c459f7744ad38a944ed1e37c
zty3075@gmail.com <zty3075@gmail.com@2a0f7078-9aa0-a7c8-291c-ce4e4205d19b>
2013-11-28 11:45:40 +0000

    Added function for converting arc length to motor left, motor right constants and time. Due to how the motor functions work, only multiples are given. (Discuss with me for understanding how the new motors work)

--- deadreckoningNew.py
+++ deadreckoningNew.py
@@ -2,14 +2,23 @@ from util import *
 import time
 import threading
 import math
-DISTANCE_PER_COUNT = 0.49833887#Millimeters
+DISTANCE_PER_COUNT = 0.511#0.511864406#0.51090604#0.491493382#Millimeters
+COUNTS_PER_METER = 1000.0/DISTANCE_PER_COUNT
 ROBOT_DIAMETER = 142.5#Measurement in millimeters
 DEFAULT_DIGITS = 6
 PRINT_COORDINATES = False
 UPDATE_INTERVAL = 0.1#Seconds
 SPEED_CHANGE_SENSITIVITY = 2#in number of update intervals, lower = more sensitive
 runThread = True
-
+motorValues = [[0.0,0]]
+for i in range(19,107,8):
+    motorValues.append([float(i)/-100.0,-1.0*((i-19.0)/8.0*48.0+113.0)])
+    motorValues.append([float(i)/100.0,(i-19.0)/8.0*48.0+113.0])
+multiplierValues = []
+for value in motorValues:
+    multiplierValues.append([value[0],float(value[1])/float(motorValues[len(motorValues)-1][1])])
+    #print value[0]
+    #print float(value[1])/float(motorValues[len(motorValues)-1][1])
 x_pos = 0.0#In millimeters
 y_pos = 0.0#In millimeters
 robotHeading = 0.0
@@ -17,6 +26,16 @@ moveHistory = []
 beginTime = time.time()
 previousTime = time.time()
 lastMove = 0#number of update intervals ago
+
+def getClosestMotorMultiplierValues(multiplierValue):
+    diff = math.fabs(multiplierValues[0][1]-multiplierValue)
+    position = 0
+    for i in range(1,len(multiplierValues)):
+        if math.fabs(multiplierValues[i][1]-multiplierValue)<diff:
+            diff = math.fabs(multiplierValues[i][1]-multiplierValue)
+            position = i
+    return multiplierValues[position]
+        
 
 #resets coordinates
 def reset_deadReckoning():
@@ -61,7 +80,7 @@ def update():
     distanceRight = float(encoders[1])*DISTANCE_PER_COUNT/2.0#gets distance travelled by right wheel
     delta_t=time.time()-previousTime
     moveHistory.append([distanceLeft*0.001,distanceRight*0.001,delta_t])
-    state['trail']=distanceLeft*0.001,distanceRight*0.001
+    #state['trail']=distanceLeft*0.001,distanceRight*0.001
     cosCurrentAngle = math.cos(robotHeading)#cosine of current angle
     sinCurrentAngle = math.sin(robotHeading)#sine of current angle
     if(encoders[0]==encoders[1]):#if the distances are equal (robot is going straight)
@@ -74,10 +93,10 @@ def update():
         y_pos += distRatio * (math.sin(distDiff/ROBOT_DIAMETER+robotHeading)-sinCurrentAngle)#updates global coordinate y
         robotHeading += distDiff/ROBOT_DIAMETER#updates current robot heading
     while robotHeading > math.pi:
-        robotHeading -= 2*math.pi
+        robotHeading -= 2.0*math.pi
     while robotHeading < -math.pi:
-        robotHeading += 2*math.pi
-    state['where']=getX(),getY(),getHeading()
+        robotHeading += 2.0*math.pi
+    #state['where']=getX(),getY(),getHeading()
 
 def stopThread():
     global runThread
@@ -85,7 +104,7 @@ def stopThread():
 
 #gets coordinates
 def getCoords(numberOfDigits=DEFAULT_DIGITS):
-    print "(" + str(round(x_pos*0.001,numberOfDigits)) + "," + str(round(y_pos*0.001,numberOfDigits)) + " at angle "+str(round(robotHeading,numberOfDigits))+")"
+    print "(" + str(round(getX(),numberOfDigits)) + "," + str(round(getY(),numberOfDigits)) + " at angle "+str(round(getHeading(),numberOfDigits))+")"
 
 def withinError(a,b,maxRelativeError):
     if((b-a)/a<maxRelativeError):
@@ -100,7 +119,7 @@ def getY():
     return y_pos*0.001
 
 def getHeading():
-    return (robotHeading+3*math.pi/2)%(2*math.pi)-math.pi
+    return (float(robotHeading)+3.0*math.pi/2.0)%(2.0*math.pi)-math.pi
 
 def getSpeed(move):
     return (move[0]+move[1])/2.0/move[2]
@@ -130,8 +149,26 @@ def getRecentAverageSpeed(numberOfMoves):
 def getMoveExpected(time):
     pass#to be edited
 
-def arcLengthToTime(arcLength):#Will use a constant motor value of 1 for now
-    pass#to be edited
+def a(arcLengthLeft,arcLengthRight):#rcLengthToSpeedTime
+    if(math.fabs(arcLengthRight-arcLengthLeft)<0.00001):
+        countsLeft = COUNTS_PER_METER*arcLengthLeft
+        countsRight = COUNTS_PER_METER*arcLengthRight
+        motorsRight = 1.0
+        motorsLeft = 1.0
+        timeTaken = float(arcLengthRight) * COUNTS_PER_METER / motorValues[len(motorValues)-1][1] * 2
+    elif(arcLengthRight>arcLengthLeft):
+        countsRight = COUNTS_PER_METER*arcLengthRight
+        countsLeft = countsRight*getClosestMotorMultiplierValues(arcLengthLeft/arcLengthRight)[1]
+        motorsRight = 1.0
+        motorsLeft = getClosestMotorMultiplierValues(arcLengthLeft/arcLengthRight)[0]
+        timeTaken = float(arcLengthRight) * COUNTS_PER_METER / motorValues[len(motorValues)-1][1] * 2
+    else:
+        countsLeft = COUNTS_PER_METER*arcLengthLeft
+        countsRight = countsLeft*getClosestMotorMultiplierValues(arcLengthRight/arcLengthLeft)[1]
+        motorsLeft = 1.0
+        motorsRight = getClosestMotorMultiplierValues(arcLengthRight/arcLengthLeft)[0]
+        timeTaken = float(arcLengthLeft) * COUNTS_PER_METER / motorValues[len(motorValues)-1][1] * 2
+    return [motorsLeft,motorsRight,timeTaken]
 
 def getMoveHistory(begin=0,end=None):#returns in reverse chronological order, end is exclusive
     '''returns list of (left_arclength,right_arclength) in reverse chronological order'''
@@ -143,4 +180,15 @@ def getMoveHistory(begin=0,end=None):#returns in reverse chronological order, en
     return output
 
 if __name__=='__main__':
-    xp_initialize()
+    #xp_initialize()
+    pass
+
+def calibrate():
+    get_encoders(False)
+    for i in range(1,51,1):
+        print float(i)/50.0
+        motors(float(i)/50.0,float(i)/50.0)
+        time.sleep(1)
+        print get_encoders(False)
+        print "__________"
+    stop()
